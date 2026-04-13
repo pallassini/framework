4ef6cfc5ec48e073241aee9f2e2d507cbf106ba7
pallassini <admin@pallassini.com>
2026-04-13 15:29:19 +0200

    jhy

--- core/client/state/index.ts
+++ core/client/state/index.ts
@@ -2,5 +2,3 @@ export { watch } from "./effect";
 export { createState } from "./state";
 export { createState as createSessionState } from "./state";
 export { createState as createPersistState } from "./state";
-export type { Signal } from "./state";
-export type { StateMap } from "./utils/store";
