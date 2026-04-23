43d59cb8ee6a03539ecafdfae711a2ee25a8c745
pallassini <admin@pallassini.com>
2026-04-23 19:33:53 +0200

    dw

--- client/routes/admin/index.tsx
+++ client/routes/admin/index.tsx
@@ -1,11 +1,17 @@
 import { auth } from "client";
+import AdminMenu from "./components/menu";
 
 export default function Admin() {
-
   return (
     <>
-      <t>Admin</t>
-      <t click={() => auth.logout()}>LOGOUT</t>
+      <div s="row children-top">
+        <div s="sticky top-0 z-40">
+          <AdminMenu />
+        </div>
+        <div s="col centerx w-100% pb-20">
+          <t>Admin</t>
+        </div>
+      </div>
     </>
   );
 }
