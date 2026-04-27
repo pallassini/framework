37e080437d4c7229e412ad0c1596ffb764cafeb3
pallassini <admin@pallassini.com>
2026-04-27 19:07:20 +0200

    dw

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -10,7 +10,12 @@ export default function Home() {
         </div>
 
         <div s="col centerx children-centerx w-100% des:(-ml-19)">
-          <div s="bg-secondary round-round des:(mt-20 px-5 py-3 w-50) mob:(mt-20 px-3 py-2 w-98%) children-centerx col"></div>
+          <div s="bg-secondary round-round des:(mt-20 px-5 py-3 w-50) mob:(mt-20 px-3 py-2 w-98%) children-centerx col">
+
+
+
+
+          </div>
         </div>
       </div>
     </>
