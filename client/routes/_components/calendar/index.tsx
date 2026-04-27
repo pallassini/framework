2943bc7c6877de192a528a46a2f54828d00a18ab
pallassini <admin@pallassini.com>
2026-04-27 22:41:48 +0200

    dw

--- client/routes/_components/calendar/index.tsx
+++ client/routes/_components/calendar/index.tsx
@@ -28,31 +28,24 @@ export const WEEKDAYS_IT = [
   "Domenica",
 ] as const;
 
-const year = state(new Date().getFullYear());
+
 export default function Calendar() {
   return (
     <div s="des:(w-80% h-85) mob:(w-100% h-74) bg-secondary round-round mt-5 col minh-0 overflow-hidden">
       {/* HEADER */}
       <div s="row centerx children-center p-4">
-        <Year />
+        <YearMonth />
       </div>
     </div>
   );
 }
 
 // ───────────────────────────────────────────────────────────────────────────────
-// YEAR
+// YEAR & MONTH
 // ───────────────────────────────────────────────────────────────────────────────
-function Year() {
-  const YEAR_MIN = 1990;
-  const YEAR_MAX = 2060;
-
-  function yearRowsAround(anchor: number, len = 9): number[] {
-    let start = anchor - 1;
-    if (start < YEAR_MIN) start = YEAR_MIN;
-    if (start + (len - 1) > YEAR_MAX) start = YEAR_MAX - (len - 1);
-    return Array.from({ length: len }, (_, i) => start + i);
-  }
+const year = state(new Date().getFullYear());
+const month = state(new Date().getMonth());
+function YearMonth() {
 
   const closePulse = state(0);
 
@@ -61,20 +54,26 @@ function Year() {
       <Popmenu
         mode="light"
         direction="bottom"
-        collapsed={() => <t s="px-4 py-2 row text-3 font-6">{year}</t>}
+        collapsed={() => <t s="px-4 py-2 row text-3 font-6">{()=>MONTHS_IT[month]} {year}</t>}
         closePulse={() => closePulse()}
         extended={() => (
-          <div s="p-4 col-3">
-            <For each={yearRowsAround(year())}>
+          <div s="p-4 col-3 col children-centerx gap-2">
+            <div s="row centerx gap-2 children-center">
+                <icon size="6" name="chevronLeft" stroke="3" s="hover:(bg-#a6a6a6 scale-120 ) px-2 py-1 round-10px" click={() => year(year() - 1)}/>
+                <t s="text-5 font-6">{year}</t>
+                <icon size="6" name="chevronRight" stroke="3" s="hover:(bg-#a6a6a6 scale-120 ) px-2 py-1 round-10px" click={() => year(year() + 1)}/>
+            </div>
+         <div s='col-4'>
+            <For each={MONTHS_IT}>
               {(y) => (
                 <t
                   s={() =>
-                    y === year()
-                      ? "px-2 py-1 mx-1 my-1 row text-3 scale-105 font-6 bg-primary cursor-pointer round-10px "
-                      : "px-2 py-1 mx-1 my-1 row text-3 cursor-pointer hover:(scale-105 bg-#a6a6a6) round-10px"
+                    y === MONTHS_IT[month]
+                      ? "px-2 py-2  row text-3 font-6 bg-primary cursor-pointer round-10px "
+                      : "px-2 py-2 row text-3 font-6 cursor-pointer hover:(scale-105 bg-#a6a6a6) round-10px"
                   }
                   click={() => {
-                    year(y);
+                    month(MONTHS_IT.indexOf(y));
                     closePulse(1);
                     setTimeout(() => closePulse(0), 100);
                   }}
@@ -83,9 +82,11 @@ function Year() {
                 </t>
               )}
             </For>
+         </div>
           </div>
         )}
       />
     </>
   );
 }
+
